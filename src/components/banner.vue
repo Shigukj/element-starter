bf93ccf4346c2cd4ea3f2cdb1fc59a79744c1711
shigubaba <18258771957@163.com>
2020-11-18 18:35:50 +0800

    首页推荐

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -1,8 +1,27 @@
 <template>
-    <el-main style="padding-top:36px;">
+    <el-main style="padding: 36px 0px 40px 0px;">
         <el-container class="banner-box">
             <el-aside width="216px" class="banner-box-left">
-                
+                <div  v-for="(item,index) in items" :key=index :class="hovermun === index?'item item-on':'item'"  @mouseenter="munhover(index)" @mouseleave="munout()">
+                    <el-link :underline="false" >
+                         <span class="group">{{item.munname}}</span>
+                         <i class="el-icon-caret-right"></i>
+                    </el-link>
+                </div>
+                <div v-for="(item,index) in items" :key=item.name v-show="hovermun == index" class="submenu"  @mouseenter="munhover(index)" @mouseleave="munout()">
+                        <div class="innerBox">
+                            <h6>{{item.groupname}}</h6>
+                            <el-divider></el-divider>
+                            <el-row>
+                                <el-link
+                                    v-for="tag in item.list"
+                                    :key="tag.name" :underline="false" :href="tag.url">
+                                    {{ tag.name }}
+                                </el-link>
+                            </el-row>
+                           
+                        </div>
+                </div>
             </el-aside>
             <el-main class="banner-box-right">
                 <el-carousel height="382px">
@@ -19,33 +38,132 @@ export default {
     data(){
         return{
             bannerUrls:['https://img.mukewang.com/5eeb15170001b36418720764.jpg','https://img.mukewang.com/5facde180001f82518720764.jpg'],
+            items:[
+                {   
+                    munname:'前沿 / 区块链 / 人工智能',
+                    groupname:'前沿技术',
+                    list:[
+                        {
+                            name:'区块链',
+                            url:'http://www.baidu.com'
+                        },
+                        {
+                            name:'微服务',
+                            url:'www.baidu.com'
+                        },
+                    ]
+                },
+                {   
+                    munname:'前端 / 小程序 / JS',
+                    groupname:'前端技术',
+                    list:[
+                        {
+                            name:'vue',
+                            url:'www.baidu.com'
+                        },
+                        {
+                            name:'小程序',
+                            url:'www.baidu.com'
+                        },
+                    ]
+                },
+            ],
+            hovermun:null,
         }
     },
      methods: {
-      
+      munhover(index) {
+          this.hovermun = index;
+      },
+      munout(){
+           this.hovermun = null
+      }
     }
   
 }
 </script>
-<style  scoped>
+
+<style lang="less"  scoped>
 .banner-box{
     margin-top:32px;
     height: 382px;
-    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+    box-shadow: 0 12px 24px 0 rgba(7, 17, 27, 0.2);
     position: relative;
     overflow: hidden;
     width: 1152px;
     margin: auto;
     border-radius: 8px;
+    .el-menu{
+    border-right: 0;
+    }
 }
+
 .banner-box-right{
     padding: 0;
 }
+/* 左侧 */
 .banner-box-left{
     background: #2B333B;
     color: #C0C4CC;
-}
-.el-menu{
-    border-right: 0;
+    font-size: 13px;
+    padding-top: 17px;
+    box-sizing: border-box;
+    .item{
+        height: 50px;
+        line-height: 50px;
+        font-size: 12px;
+        cursor: pointer;
+        padding-left: 12px;
+        position: relative;
+        a{
+            display: block;
+            color: rgba(255, 255, 255, 0.6);
+            padding: 0 16px;
+            border-top-left-radius: 4px;
+            border-bottom-left-radius: 4px;
+            height: 50px;
+            transition: all .1s;
+            .el-link--inner i{
+                position: absolute;
+                right: 8px;
+                top: 16px;
+                color: rgba(255, 255, 255, 0.5);
+                font-size: 16px;
+            }   
+        }
+    }
+    .item-on{
+        background-color: rgba(255, 255, 255, 0.3);
+        color: #fff;
+    }
+    .submenu{
+        background: #fff;
+        position: absolute;
+        left: 216px;
+        top: 0px;
+        width: 768px;
+        height: 382px;
+        z-index: 581;
+        box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.1);
+        box-sizing: border-box;
+        .innerBox{
+            padding: 26px 36px 32px;
+            position: relative;
+            min-height: 150px;
+            .el-divider--horizontal{
+                margin: 10px 0;
+            }
+            .el-link{
+                margin-right: 24px;
+                margin-bottom: 16px;
+            }
+        }
+        h6{
+            margin: 0;
+            font-weight: 800;
+            font-size: 18px;
+            color: #1f2f3d;
+        }
+    }
 }
 </style>
--- src/view/home.vue
+++ src/view/home.vue
@@ -1,6 +1,23 @@
 <template>
-    <el-container>
-            <Banner></Banner>
+    <el-container id="home">
+        <Banner></Banner>
+        <el-main class="container-types container-recommend">
+                 <h5 class="types-title">
+                     <span>零基础就业</span>
+                    <span class="intro">
+                        从入门到具备1年开发经验
+                    </span>
+                </h5>
+                <el-row>
+                  <el-col :span="6" v-for="(item,index) in recommend_items" :key="item.title">
+                      <div  class="item">
+                        <i :class="`i-${index}`"></i>
+                        <p class="tit">{{item.title}}</p>
+                        <p class="desc">{{item.desc}}</p>
+                      </div>
+                  </el-col>
+                </el-row>
+        </el-main>
         <el-footer>
         </el-footer>
     </el-container>
@@ -9,6 +26,28 @@
 import Banner from '../components/banner'
  
 export default {
+    data(){
+        return{
+            recommend_items:[
+                {
+                    title:"Java工程师体系课",
+                    desc:"从0到胜任大厂Java岗位"
+                },
+                {
+                    title:"Java工程师体系课1",
+                    desc:"从0到胜任大厂Java岗位"
+                },
+                  {
+                    title:"Java工程师体系课2",
+                    desc:"从0到胜任大厂Java岗位"
+                },
+                  {
+                    title:"Java工程师体系课3",
+                    desc:"从0到胜任大厂Java岗位"
+                },
+            ],
+        }
+    },
     components: {
     Banner
   },
@@ -17,6 +56,92 @@ export default {
   }
 }
 </script>
-<style>
+<style lang="less" >
+@boxwidth:1152px;
+    .container-types {
+        width: @boxwidth;
+        margin: auto;
+        &.container-recommend{
+            padding-top: 0px;
+            padding-bottom: 44px;
+        }
+      
+        .types-title{
+            margin: 0;
+            position: relative;
+            font-size: 24px;
+            color: #1C1F21;
+            letter-spacing: 0px;
+            line-height: 32px;
+            .intro{
+                margin-top: 6px;
+                margin-left: 12px;
+                font-weight: normal;
+                font-size: 14px;
+                color: #545C63;
+                line-height: 20px;
+            }
+        }
+        .item{
+            margin-left: 24px;
+            font-size: 0;
+            display: block;
+            width: 270px;
+            padding-top: 20px;
+            box-sizing: border-box;
+            height: 90px;
+            overflow: hidden;
+            transition: all .3s;
+            background: #F3F5F6;
+            border-radius: 8px;
+            position: relative; 
+            &:first-child {
+                margin-left: 0;
+            }
+            i{
+                float: left;
+                width: 50px;
+                height: 50px;
+                background: url("https://www.imooc.com/static/img/index/new/path-2019-9.png") no-repeat;
+                background-size: 100%;
+                margin-left: 16px;
+                margin-right: 12px;
+                &.i-0{
+                    background-position: center -50px;
+                }
+                &.i-1{
+                    background-position: center 0px;
+                }
+                &.i-2{
+                    background-position: center -100px;
+                }
+                &.i-3{
+                    background-position: center -150px;
+                }
+            }
+            .tit{
+                font-size: 18px;
+                font-weight: bold;
+                color: #1C1F21;
+                margin-top: 0px;
+            }
+            .desc{
+                font-size: 14px;
+                color: #1C1F21;
+                line-height: 16px;
+                margin-top: 5px;
+                font-weight: 400; 
+            }
+            &:hover{
+                box-shadow: 0px 4px 8px #0000001f;
+            }
+        }
+        .el-row{
+            margin-top: 24px;
+        }
+        
+        
+    }
+    
 
 </style>
